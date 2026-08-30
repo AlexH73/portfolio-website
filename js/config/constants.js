66b3a3a27fe1f0934ac2d929a12542b4a59bc0e9
Alexander Hermann <ewebotah@gmail.com>
2026-08-30 14:51:48 +0200

    fix(portfolio): serve localized CV files

--- js/config/constants.js
+++ js/config/constants.js
@@ -70,20 +70,20 @@ export const LANGUAGES = {
   de: {
     name: 'Deutsch',
     ogLocale: 'de_DE',
-    resumeUrl:
-      'https://docs.google.com/document/d/1-Efl8M1ISj_uN26r-S0XirIe5XR7FAXGnyk4y6BYw6w/edit?tab=t.0',
+    resumeUrl: new URL('../../docs/cv/alexander-hermann-cv-de.pdf', import.meta.url)
+      .href,
   },
   en: {
     name: 'English',
     ogLocale: 'en_US',
-    resumeUrl:
-      'https://docs.google.com/document/d/1FyihabADRkwHqwJhvDPDL__LWzB7R6SwtRgGc_YFbjc/edit?tab=t.0',
+    resumeUrl: new URL('../../docs/cv/alexander-hermann-cv-en.pdf', import.meta.url)
+      .href,
   },
   ru: {
     name: 'Русский',
     ogLocale: 'ru_RU',
-    resumeUrl:
-      'https://docs.google.com/document/d/1NjeljZy1L2f5NMbwwDG8C_DDIPXV2ikmRNttAMdzWGc/edit?tab=t.0',
+    resumeUrl: new URL('../../docs/cv/alexander-hermann-cv-ru.pdf', import.meta.url)
+      .href,
   },
 };
 
